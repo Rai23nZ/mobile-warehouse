c7c2874b4697ab3e10f634b042d66bee4c7f9108
Rai23nZ <67661030+Rai23nZ@users.noreply.github.com>
2026-07-11 21:37:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-self.addEventListener('fetch', function(event) {});
+// Принудительная немедленная установка
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+// Активация и захват контроля над страницей
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Обязательный перехватчик сети (оставляем пустым, чтобы пропускать запросы)
+self.addEventListener('fetch', (event) => {
+    // Chrome требует наличия этого события для признания PWA
+});
